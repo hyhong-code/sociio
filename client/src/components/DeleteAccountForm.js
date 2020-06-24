0b041d4f56e808502befdc75245b46893982b58f
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-24 03:29:35 -0700

    add DeleteAccountForm

---
+++ client/src/components/DeleteAccountForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const DeleteAccountForm = () => {
+  return (
+    <div className="col-md-4 mb-4 d-flex flex-column justify-content-center align-items-center py-5 rounded">
+      <form action="">
+        <p className="lead">DELETE ACCOUNT</p>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text">
+              <i className="fas fa-exclamation-circle text-danger"></i>
+            </span>
+          </div>
+          <input type="text" className="form-control" placeholder="Password" />
+        </div>
+        <small className="d-block mb-3 text-danger">
+          Your account won't be recovered.
+        </small>
+        <a href="" className="btn btn-success px-3">
+          Back
+        </a>
+        <input
+          type="submit"
+          className="btn btn-danger"
+          value="Delete Account"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default DeleteAccountForm;
