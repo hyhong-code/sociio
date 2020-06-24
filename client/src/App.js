0322fb05e3121417e6040e06b0fd87a4fdcec4f9
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-24 04:43:41 -0700

    set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,17 @@ import Signup from './pages/Signup';
 import Profile from './pages/Profile';
 import './App.css';
 
-
-
 const App = () => {
   return (
     <BrowserRouter>
       <Fragment>
         <Navbar />
         <Switch>
-          {/* <Landing /> */}
-          {/* <Home /> */}
-          {/* <Comment /> */}
-          {/* <Signin /> */}
-          {/* <Signup /> */}
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/comment" component={Comment} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
           <Profile />
         </Switch>
       </Fragment>
