59f1e952d63f859a67b0411ebca933175618b994
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-24 03:35:50 -0700

    add UpdateInfoForm

---
+++ client/src/components/UpdateInfoForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const UpdateInfoForm = () => {
+  return (
+    <div className="col-md-4 mb-4 d-flex flex-column justify-content-center align-items-center py-5 rounded">
+      <form action="">
+        <p className="lead">UPDATE INFO</p>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text">@</span>
+          </div>
+          <input type="text" className="form-control" placeholder="Handle" />
+        </div>
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Name" />
+        </div>
+        <div className="form-group">
+          <input type="email" className="form-control" placeholder="Email" />
+        </div>
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Address" />
+        </div>
+        <small className="d-block mb-3">Your address wont't be shared.</small>
+        <a href="" className="btn btn-secondary px-3">
+          Back
+        </a>
+        <input type="submit" className="btn btn-info" value="Update" />
+      </form>
+    </div>
+  );
+};
+
+export default UpdateInfoForm;
