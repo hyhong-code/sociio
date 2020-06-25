8041aff0211e34dc50d24e71a5853edb7e9a471c
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-24 18:00:59 -0700

    add panelView actions

---
+++ client/src/actions/profileAction.js
@@ -0,0 +1,47 @@
+import {
+  GET_PROFILE,
+  CLEAR_PROFILE,
+  PROFILE_FAIL,
+  DISPLAY_PROFILE_PANEL,
+  DISPLAY_PROFILE_FOLLOWERS,
+  DISPLAY_PROFILE_FOLLOWING,
+  DISPLAY_PROFILE_EDITINFO,
+  DISPLAY_PROFILE_EDITPASSWORD,
+  DISPLAY_PROFILE_DELETEACCOUNT,
+} from './profileAction';
+
+export const displayProfilePanel = () => (dispatch) => {
+  dispatch({
+    type: DISPLAY_PROFILE_PANEL,
+  });
+};
+
+export const displayProfileFollowing = () => (dispatch) => {
+  dispatch({
+    type: DISPLAY_PROFILE_FOLLOWING,
+  });
+};
+
+export const displayProfileFollowers = () => (dispatch) => {
+  dispatch({
+    type: DISPLAY_PROFILE_FOLLOWERS,
+  });
+};
+
+export const displayProfileEditInfo = () => (dispatch) => {
+  dispatch({
+    type: DISPLAY_PROFILE_EDITINFO,
+  });
+};
+
+export const displayProfileEditPassword = () => (dispatch) => {
+  dispatch({
+    type: DISPLAY_PROFILE_EDITPASSWORD,
+  });
+};
+
+export const displayProfileDelectAccount = () => (dispatch) => {
+  dispatch({
+    type: DISPLAY_PROFILE_DELETEACCOUNT,
+  });
+};
